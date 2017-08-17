a75ca1c9995d2294b1a2eff3756f2b30ad15beeb
Asenka <rbourreau@gmail.com>
2017-08-17 22:13:07 +0200

    Update MtgAbility

--- workspace/MtgFree/src/asenka/mtgfree/model/MtgAbility.java
+++ workspace/MtgFree/src/asenka/mtgfree/model/MtgAbility.java
@@ -1,19 +1,11 @@
 package asenka.mtgfree.model;
 
 
-import java.util.*;
-
 /**
  * This class represents a card ability (fly, deathtouch, hexproof, etc...)
  * @author Asenka
  */
 public class MtgAbility {
-
-	/**
-	 * Default constructor
-	 */
-	public MtgAbility() {
-	}
 
 	/**
 	 * 
@@ -29,5 +21,83 @@ public class MtgAbility {
 	 * A description of the ability (localized)
 	 */
 	private String description;
+	
+	
+	/**
+	 * Default constructor
+	 */
+	public MtgAbility() {
+		
+	}
+	
+	
+
+	/**
+	 * 
+	 * @param id
+	 * @param name
+	 * @param description
+	 */
+	public MtgAbility(int id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+
+
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	} 
+
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
+	
+	
 
 }
