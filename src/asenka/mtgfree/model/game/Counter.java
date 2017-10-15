e8106ee86d8e2e15a7ce911c607a7d39a3c7d6d8
AsenkaGit <rbourreau@gmail.com>
2017-10-15 22:39:25 +0200

    Add javadoc, and equals/hascode methods

--- src/asenka/mtgfree/model/game/Counter.java
+++ src/asenka/mtgfree/model/game/Counter.java
@@ -12,42 +12,76 @@ public class Counter {
 	/**
 	 * The value display by the counter (it could be empty)
 	 */
-	private String value;
+	private final String value;
 
 	/**
 	 * The color of the counter
 	 */
-	private Color color;
+	private final Color color;
 
+	/**
+	 * 
+	 * @param value
+	 * @param color
+	 */
 	public Counter(String value, Color color) {
 		super();
 		this.value = value;
 		this.color = color;
 	}
 
+	/**
+	 * @return
+	 */
 	public String getValue() {
 
 		return value;
 	}
 
-	public void setValue(String value) {
-
-		this.value = value;
-	}
-
+	/**
+	 * @return
+	 */
 	public Color getColor() {
 
 		return color;
-	}
-
-	public void setColor(Color color) {
-
-		this.color = color;
 	}
 
 	@Override
 	public String toString() {
 
 		return "MtgCounter [" + value + ", " + color + "]";
+	}
+
+	@Override
+	public int hashCode() {
+
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Counter other = (Counter) obj;
+		if (color == null) {
+			if (other.color != null)
+				return false;
+		} else if (!color.equals(other.color))
+			return false;
+		if (value == null) {
+			if (other.value != null)
+				return false;
+		} else if (!value.equals(other.value))
+			return false;
+		return true;
 	}
 }
