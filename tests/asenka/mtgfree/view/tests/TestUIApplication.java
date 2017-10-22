81424728f84c138573286084a40b6e018d4a2e2c
AsenkaGit <rbourreau@gmail.com>
2017-10-22 11:42:27 +0200

    Create Test UI interface (JavaFX FXML witht default components)

---
+++ tests/asenka/mtgfree/view/tests/TestUIApplication.java
@@ -0,0 +1,21 @@
+package asenka.mtgfree.view.tests;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TestUIApplication extends Application {
+
+	private Stage primaryStage;
+	
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
