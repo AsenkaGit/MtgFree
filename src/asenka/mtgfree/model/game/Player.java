6741d4f99deff9cab026ee7b9a218714b6aa60e6
AsenkaGit <rbourreau@gmail.com>
2017-10-31 16:30:53 +0100

    Add hashCode and equals to Player class

--- src/asenka/mtgfree/model/game/Player.java
+++ src/asenka/mtgfree/model/game/Player.java
@@ -426,6 +426,49 @@ public class Player extends Observable implements Serializable, Comparable<Playe
 			super.notifyObservers(new PlayerEvent("clear", "exile", null));
 		}
 	}
+	
+	
+
+	@Override
+	public String toString() {
+
+		return "Player [" + name + ", " + lifeCounters + ", " + poisonCounters + "]";
+	}
+	
+	
+
+	@Override
+	public int hashCode() {
+
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((selectedDeck == null) ? 0 : selectedDeck.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Player other = (Player) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (selectedDeck == null) {
+			if (other.selectedDeck != null)
+				return false;
+		} else if (!selectedDeck.equals(other.selectedDeck))
+			return false;
+		return true;
+	}
 
 	@Override
 	public int compareTo(Player otherPlayer) {
