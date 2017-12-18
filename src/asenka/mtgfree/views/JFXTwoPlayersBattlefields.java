f8b2f109ab1f7442b67ac2a8444c173fc960dd2b
AsenkaGit <rbourreau@gmail.com>
2017-12-18 15:04:26 +0100

    Add javadoc on JFXTwoPlayerBattlefiels

--- src/asenka/mtgfree/views/JFXTwoPlayersBattlefields.java
+++ src/asenka/mtgfree/views/JFXTwoPlayersBattlefields.java
@@ -4,12 +4,30 @@ import asenka.mtgfree.controllers.GameController;
 import javafx.geometry.Orientation;
 import javafx.scene.control.SplitPane;
 
+/**
+ * This components display two JFXBattlefield in a vertical split pane.
+ * 
+ * @author asenka
+ * @see SplitPane
+ * @see JFXBattlefield
+ */
 public class JFXTwoPlayersBattlefields extends SplitPane {
 	
+	/**
+	 * The battlefield displaying the cards of the local player
+	 */
 	private final JFXBattlefield localPlayerBattlefield;
 	
+	/**
+	 * The battlefield displaying the cards of the opponent
+	 */
 	private final JFXBattlefield otherPlayerBattlefield;
 	
+	/**
+	 * Build a vertical split pane with two JFXBattlefield
+	 * @param controller the game controller
+	 * @see JFXBattlefield
+	 */
 	public JFXTwoPlayersBattlefields(final GameController controller) {
 		
 		super();
